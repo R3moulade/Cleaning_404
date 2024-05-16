120d98d27f602a906fdcb152faeffc5cf185b6b6
Enikő Valkó <eaaenva@students.eaaa.dk>
2024-05-16 11:09:45 +0200

    Set up the first person movement

---
+++ Cleaning_404/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY; // usually subtract to get natural up/down movement
+
+        // restrict looking too up or too down
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
