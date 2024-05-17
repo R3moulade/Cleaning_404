a5d607f306eefb78c8e3e1c2f6cdbc79071f8285
TineMSPetersen <111304307+TineMSPetersen@users.noreply.github.com>
2024-05-17 13:28:29 +0200

    Remove trash

---
+++ Cleaning_404/Assets/Scripts/Trash Remover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashRemover : MonoBehaviour
+{
+
+    public string trashTag;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Raycast from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Hit something!");
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                 Destroy(hit.collider.gameObject);
+             }
+        }
+    }
+}
