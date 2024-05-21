081aee18a51c1965b415568148efaea07ec591cd
TineMSPetersen <111304307+TineMSPetersen@users.noreply.github.com>
2024-05-21 11:20:04 +0200

    Spawn pentagram when no dirt

--- Cleaning_404/Assets/Scripts/GameManager.cs
+++ Cleaning_404/Assets/Scripts/GameManager.cs
@@ -5,33 +5,41 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameObject objectToSpawn;
+    public Transform spawnLocation;
 
     public string checkDirt;
+
+    private bool objectSpawned = false;
 
     void Update() {
         int count = CountDirt(checkDirt);
 
-
-        if (count == 0) {
-            Debug.Log("Wahoo");
+        if (count == 0 && objectSpawned == false) {
+                SpawnObject();
+                objectSpawned = true;
         }
     }
 
     private void Awake() {
         if (instance == null) {
             instance = this;
-
             DontDestroyOnLoad(gameObject);
-        }
-
-        else {
+        } else {
             Destroy(gameObject);
         }
     }
 
     int CountDirt(string tag) {
         GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
-
         return taggedObjects.Length;
+    }
+
+    void SpawnObject() {
+        if (objectToSpawn != null && spawnLocation != null) {
+            Instantiate(objectToSpawn, spawnLocation.position, spawnLocation.rotation);
+        } else {
+            Debug.LogError("Missing objectToSpawn or spawnLocation");
+        }
     }
 }
