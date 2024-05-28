181891a5813f94c83a6fec9e5ac0114258bf8f55
Enikő Valkó <eaaenva@students.eaaa.dk>
2024-05-28 13:36:39 +0200

    Load Scene :alien:

--- Cleaning_404/Assets/Scripts/SceneManager.cs
+++ Cleaning_404/Assets/Scripts/SceneManager.cs
@@ -1,14 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Update is called once per frame
-    public void PlayGame()
+    public static MainMenu Instance { get; private set; }
+    public int sceneNumber = 1;
+
+    private void Awake()
     {
-        SceneManager.LoadSceneAsync(1);
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    public void LoadScene()
+    {
+        SceneManager.LoadSceneAsync(sceneNumber);
     }
 
     public void QuitGame()
